fc23fa6fd2df9cdb759e53087d47be24d5362424
肖燕群 <1129429967@qq.com>
2019-07-30 17:52:25 +0800

    导航列表和商品列表

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,11 @@
 import {request} from '../../request/request'
 Page({
   data: {
-    swiperList:[]
+    swiperList:[],
+    navList:[],
+    goodList:[]
   },
+  // 请求轮播图
   getSwiperList(){
     request({url:'/home/swiperdata'})
     .then(res=>{
@@ -11,9 +14,29 @@ Page({
        })
     })
   },
+  // 请求导航数据
+  getGoodList(){
+    request({url:'/home/floordata'})
+    .then(res=>{  
+      this.setData({
+        goodList:res
+      })
+    })
+  },
+  // 请求商品信息
+  getNavList(){
+    request({url:'/home/catitems'})
+    .then(res=>{  
+      this.setData({
+        navList:res
+      })
+    })
+  },
   //options(Object)
   onLoad: function(options) {
     this.getSwiperList()
+    this.getNavList()
+    this.getGoodList()
   },
 });
   
