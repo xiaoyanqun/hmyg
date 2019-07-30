6ea56314bf287fabf4422c9f511fd4d1fe8fd5c5
肖燕群 <1129429967@qq.com>
2019-07-30 20:09:42 +0800

    分类页面的数据渲染切换

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,71 @@
-// pages/category/index.js
+import {request} from '../../request/request'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    CategoriesList:[],
+    ChildList:[],
+    activeIndex:0,
+    scrollTop:0
+  },
+  AllCategoriesList:[],
+  //获取大分类数据
+  getAllCategoriesList(){
+    request({url:"/categories"})
+    .then(res=>{
+      this.AllCategoriesList =res
+      const arr = this.AllCategoriesList.map(v=>{
+        const obj = {
+          cat_id:v.cat_id,
+          cat_name:v.cat_name
+        }
+        return obj
+      })
+      this.setData({
+        CategoriesList:arr,
+        ChildList:this.AllCategoriesList[this.data.activeIndex].children
+      })
+    })
+  },
+  // 点击分类
+  handleIndex(e){
+    this.setData({
+      activeIndex: e.currentTarget.dataset.index
+    })
+    this.getAllCategoriesList()
+    this.setData({
+      scrollTop:0
+    })
+  },
+  //options(Object)
+  onLoad: function(options) {
+    this.getAllCategoriesList()
+  },
+  onReady: function() {
+    
+  },
+  onShow: function() {
+    
+  },
+  onHide: function() {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  onUnload: function() {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  onPullDownRefresh: function() {
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  onReachBottom: function() {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+  onShareAppMessage: function() {
 
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+  onPageScroll: function() {
 
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  //item(index,pagePath,text)
+  onTabItemTap:function(item) {
 
   }
-})
+});
+  
