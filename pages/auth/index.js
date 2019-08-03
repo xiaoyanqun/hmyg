46590273b233e21c3b308d53aafb9a431168d084
肖燕群 <1129429967@qq.com>
2019-08-04 00:04:21 +0800

    用户授权，登录

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,4 +1,6 @@
-// pages/auth/index.js
+import {login} from '../../lib/wxAsync/wxAsync'
+import {request} from '../../request/request'
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
 
   /**
@@ -7,60 +9,16 @@ Page({
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  async handleClike(e){
+    console.log(e)
+    const {encryptedData,rawData,iv,signature} = e.detail
+    const {code} = await login()
+    const obj = {encryptedData,rawData,iv,signature,code}
+    const res1 = await request({url:"/users/wxlogin",method:'post',data:obj})
+    console.log(res1)
+    // wx.navigateTo({
+    //   url: '/pages/pay/index',
+    // });
+      
   }
 })
--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,66 +1,60 @@
-// pages/pay/index.js
+import regeneratorRuntime from '../../lib/runtime/runtime'
+import {getUserInfo} from  '../../lib/wxAsync/wxAsync'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    user:{},
+    cats:{},
+    totalPrice:0,  //总价
+    totalNum:0,   //总数量
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 支付
+  async handleClose(){
+   const token =  wx.getStorageSync('token')
+   if(!token){
+      // 2.1 跳转到 授权页面
+      wx.navigateTo({
+        url: '/pages/auth/index',
+      });
+   }else{
+    console.log('正常')
+   }
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    const user = wx.getStorageSync('user') || {};
+    const cats = wx.getStorageSync('cats') || {};
+    this.setData({
+      user,
+      cats
+    })
+    this.setCart(cats);
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  setCart(cats){
+   let catsArr =  Object.values(cats)
+      //1 计算全选
+     // 2 计算总的价格 
+     let totalPrice = 0;
+     // 3 计算 要购买的总数量
+     let totalNum = 0;
+   catsArr.forEach(v => {
+    //  计算选择商品的价格
+     if(v.isSelect){
+      totalPrice += v.goods_price * v.num
+      totalNum += v.num
+     }
+   });
+   this.setData({
+    totalPrice,
+    totalNum,
+    cats,
+   })
+   wx.setStorageSync('cats', cats);
+     
   }
 })
